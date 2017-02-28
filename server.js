5bbd171505d979560b07e7cda4b9e9cd7a353302
dhanajayank <dhanajayan59@gmail.com>
2017-02-28 17:59:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'art.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'danny.jpg'));
 });
 
 
